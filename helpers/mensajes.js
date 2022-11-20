20e93f78b128bb4e3adfb8ec2161226337310d27
kevin4moe <kevin4moe@gmail.com>
2022-11-19 21:50:18 -0600

    di end

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,30 +1,49 @@
+const { resolve } = require("path");
+
 require("colors");
 
 const mostrarMenu = () => {
-  console.clear();
-  console.log("________________________\n".green);
-  console.log(" Seleccione una opción".green);
-  console.log("________________________\n".green);
+  return new Promise((resolve) => {
+    console.clear();
+    console.log("________________________\n".green);
+    console.log(" Seleccione una opción".green);
+    console.log("________________________\n".green);
 
-  console.log(`${"1.".green} Crear tarea`);
-  console.log(`${"2.".green} Listar tareas`);
-  console.log(`${"3.".green} Listar tareas completadas`);
-  console.log(`${"4.".green} Listar tareas pendientes`);
-  console.log(`${"5.".green} Completar tarea(s)`);
-  console.log(`${"6.".green} Borrar tarea`);
-  console.log(`${"0.".green} Salir\n`);
+    console.log(`${"1.".green} Crear tarea`);
+    console.log(`${"2.".green} Listar tareas`);
+    console.log(`${"3.".green} Listar tareas completadas`);
+    console.log(`${"4.".green} Listar tareas pendientes`);
+    console.log(`${"5.".green} Completar tarea(s)`);
+    console.log(`${"6.".green} Borrar tarea`);
+    console.log(`${"0.".green} Salir\n`);
 
-  const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    readline.question("Seleccione una opción: ", (opt) => {
+      readline.close();
+      resolve(opt);
+    });
   });
+};
 
-  readline.question("Seleccione una opción: ", (opt) => {
-    console.log({ opt });
-    readline.close();
+const pausa = () => {
+  return new Promise((resolve) => {
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    readline.question(`Presione ${"Enter".green} para continuar`, (opt) => {
+      readline.close();
+      resolve();
+    });
   });
 };
 
 module.exports = {
   mostrarMenu,
+  pausa,
 };
